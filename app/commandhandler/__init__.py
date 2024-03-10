e617a41dbbb4a8ad868218e1c1ebf146981d71a1
whua0 <wh44@njit.edu>
2024-03-10 19:03:57 -0400

    Created Class Command, Class CommandHandler

---
+++ app/commandhandler/__init__.py
@@ -0,0 +1,35 @@
+'''Command Hanager'''
+import logging
+from abc import ABC, abstractmethod
+
+class Command(ABC):
+    '''Class Command'''
+
+    @abstractmethod
+    def execute(self):
+        '''Any subclass of Command must implement its own execute method'''
+
+class CommandHandler:
+    '''Class CommandHandler'''
+
+    def __init__(self):
+        '''Constructor for a Commands Dictionary'''
+        self.commands = {}
+
+    def register_command(self, command_name: str, command: Command):
+        '''Registers the Command'''
+        self.commands[command_name] = command
+        logging.info("Registered Command '%s' from Plugin '%s'.", command_name, command)
+
+    def execute_command(self, command_name: str):
+        '''Executes the Command'''
+        try:
+            logging.info("Attempted Command: '%s'.", command_name)
+            self.commands[command_name].execute()
+            logging.info("Executed Command: '%s'.", command_name)
+
+        except KeyError:
+            logging.warning("Invalid Command: '%s'.", command_name)
+            print(f"Invalid Command: {command_name}")
+            print("Type 'menu' for list of commands.")
+            print("Type 'exit' to exit.")
