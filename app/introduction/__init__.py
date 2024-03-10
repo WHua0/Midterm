f98db36e1bf1c102b31253ab9fb1142faf4ad3ba
whua0 <wh44@njit.edu>
2024-03-10 19:50:42 -0400

    Added Logging

--- app/introduction/__init__.py
+++ app/introduction/__init__.py
@@ -1,6 +1,8 @@
 '''Introduction'''
+import logging
 
 def introduction():
     '''Prints Introduction Message'''
     print("Calculator App Initiated ...")
     print("Please type a command.")
+    logging.info("Printed Introduction")
