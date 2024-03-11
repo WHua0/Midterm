6f1cd2a175386db2eedbab4eaf1440e0c98066e5
whua0 <wh44@njit.edu>
2024-03-11 16:28:55 -0400

    Tested Class AddCommand

---
+++ tests/tests_plugins/test_add.py
@@ -0,0 +1,28 @@
+# pylint: disable = unused-argument
+
+'''Test Add'''
+import unittest
+from unittest.mock import patch
+from app.plugins.add import AddCommand
+
+class TestAddCommand(unittest.TestCase):
+    '''Tests AddCommand'''
+
+    @patch("builtins.input", side_effect=["3 5"])
+    def test_execute_with_correct_inputs(self, mock_input):
+        '''Tests Execute_AddCommand with Correct Number of Inputs'''
+        command = AddCommand()
+        with patch("builtins.print") as mock_print:
+            command.execute()
+        mock_print.assert_called_once_with("The result of 3 add 5 is equal to 8.")
+
+    @patch("builtins.input", side_effect=["3"])
+    def test_execute_with_incorrect_inputs(self, mock_input):
+        '''Tests Execute_AddCommand with Incorrect Number of Inputs'''
+        command = AddCommand()
+        with patch("builtins.print") as mock_print:
+            command.execute()
+        mock_print.assert_called_once_with("An error occurred: incorrect number of inputs.")
+
+if __name__ == '__main__':
+    unittest.main() # pragma: no cover
