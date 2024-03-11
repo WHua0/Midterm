470c6d8ac16e89cb2a0dbfe8ed3ec44be5941394
whua0 <wh44@njit.edu>
2024-03-11 16:28:21 -0400

    Created Class AddCommand

---
+++ app/plugins/add/__init__.py
@@ -0,0 +1,20 @@
+'''Add'''
+import logging
+from app.commandhandler import Command
+from app.calculator import Calculator
+
+class AddCommand(Command):
+    '''Add Command'''
+
+    def execute(self):
+        values = input("Please provide the two numbers separated by a space: ").split()
+
+        if len(values) == 2:
+            a, b = values[0], values[1]
+            Calculator.calculate_and_print(a, b, "add")
+            logging.info("Inputs: '%s', '%s'.", a, b)
+
+        else:
+            print(f"An error occurred: incorrect number of inputs.")
+            values_string = " ".join(map(str, values))
+            logging.warning("Incorrect number of inputs: '%s'.", values_string)
