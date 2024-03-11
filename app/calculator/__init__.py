c5c3abe9e9a27d299e8d2c2addfa22953741898c
whua0 <wh44@njit.edu>
2024-03-10 23:20:17 -0400

    Added execute, add, subtract, multiple, divide to Class Calculator

---
+++ app/calculator/__init__.py
@@ -0,0 +1,33 @@
+'''Calculator Facade'''
+from decimal import Decimal
+from app.calculator.calculation import Calculation
+from app.calculator.operations import Operation
+
+class Calculator:
+    '''Class Calculator'''
+
+    @staticmethod
+    def execute(a: Decimal, b: Decimal, operation) -> Decimal:
+        '''Calculator.execute(a, b, operation) '''
+        calculation = Calculation(a, b, operation)
+        return calculation.compute()
+
+    @staticmethod
+    def add(a: Decimal, b: Decimal) -> Decimal:
+        '''Calculator.add(a, b)'''
+        return Calculator.execute(a, b, Operation.add)
+
+    @staticmethod
+    def subtract(a: Decimal, b: Decimal) -> Decimal:
+        '''Calculator.subtract(a, b)'''
+        return Calculator.execute(a, b, Operation.subtract)
+
+    @staticmethod
+    def multiply(a: Decimal, b: Decimal) -> Decimal:
+        ''''Calculator.multiply(a, b)'''
+        return Calculator.execute(a, b, Operation.multiply)
+
+    @staticmethod
+    def divide(a: Decimal, b: Decimal) -> Decimal:
+        '''Calculator.divide(a, b)'''
+        return Calculator.execute(a, b, Operation.divide)
