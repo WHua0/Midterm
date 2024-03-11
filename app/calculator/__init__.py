beba4343cb41a5283fd7b25aa10e394bfe9893e6
whua0 <wh44@njit.edu>
2024-03-11 14:31:18 -0400

    Added calculate_and_print to Class Calculator

--- app/calculator/__init__.py
+++ app/calculator/__init__.py
@@ -1,5 +1,5 @@
 '''Calculator Facade'''
-from decimal import Decimal
+from decimal import Decimal, InvalidOperation
 from app.calculator.calculation import Calculation
 from app.calculator.operations import Operation
 
@@ -31,3 +31,30 @@ class Calculator:
     def divide(a: Decimal, b: Decimal) -> Decimal:
         '''Calculator.divide(a, b)'''
         return Calculator.execute(a, b, Operation.divide)
+
+    @staticmethod
+    def calculate_and_print(a: Decimal, b: Decimal, operation_name):
+        '''Calculates and prints result'''
+
+        operation_mappings = {
+                "add": Calculator.add,
+                "subtract": Calculator.subtract,
+                "multiply": Calculator.multiply,
+                "divide": Calculator.divide
+        }
+
+        try:
+            a_decimal, b_decimal = map(Decimal, [a, b])
+            operation_function = operation_mappings.get(operation_name)
+
+            if operation_function:
+                print(f"The result of {a} {operation_name} {b} is equal to {operation_function(a_decimal, b_decimal)}.")
+
+            else:
+                print(f"Unknown operation: {operation_name}.")
+
+        except InvalidOperation:
+            print(f"Invalid number input: {a} or {b} is not a valid number.")
+
+        except Exception as e:
+            print(f"An error occurred: {e}.")
