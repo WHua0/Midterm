feeed685db4615a96ca14ffb9e6cc05ed47857d9
whua0 <wh44@njit.edu>
2024-03-10 23:31:59 -0400

    Tested Calculator.Operation

---
+++ tests/tests_calculator/test_calculator.py
@@ -0,0 +1,29 @@
+# pylint: disable = expression-not-assigned
+
+'''Test Calculator'''
+import pytest
+from app.calculator import Calculator
+
+class TestCalculator():
+    '''Tests Calculator.Operations'''
+
+    def test_calculator_add(self):
+        '''Tests Calculator.add'''
+        assert Calculator.add(2, 2)  == 4, "Calculator Add failed!"
+
+    def test_calculator_subtract(self):
+        '''Tests Calculator.subtract'''
+        assert Calculator.subtract(2, 2)  == 0, "Calculator Subtract failed!"
+
+    def test_calculator_multiply(self):
+        '''Tests Calculator.multiply'''
+        assert Calculator.multiply(2, 2)  == 4, "Calculator Multiply failed!"
+
+    def test_calculator_divide(self):
+        '''Calculator.divide'''
+        assert Calculator.divide(2, 2)  == 1, "Calculator Divide failed!"
+
+    def test_calculator_division_by_zero(self):
+        '''Calculator.add by 0 exception'''
+        with pytest.raises(ZeroDivisionError, match = 'Cannot divide by zero'):
+            Calculator.divide(2, 0), 'Divide by Zero exception failed'
