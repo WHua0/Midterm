bb4c24f114a7136449f1fa6d1e9c750f17eda649
whua0 <wh44@njit.edu>
2024-03-10 23:51:07 -0400

    Added test for calculator.execute with faker data

--- tests/tests_calculator/test_calculator.py
+++ tests/tests_calculator/test_calculator.py
@@ -7,6 +7,10 @@ from app.calculator import Calculator
 class TestCalculator():
     '''Tests Calculator.Operations'''
 
+    def test__execute_operations(self, a, b, operation, expected):
+        '''Tests Calculator.execute with Faker'''
+        assert Calculator.execute(a, b, operation) == expected, f"{operation.__name__} operation failed"
+
     def test_calculator_add(self):
         '''Tests Calculator.add'''
         assert Calculator.add(2, 2)  == 4, "Calculator Add failed!"
