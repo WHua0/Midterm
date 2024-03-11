106c713512c52354f203170bb895333163607eac
whua0 <wh44@njit.edu>
2024-03-11 18:53:55 -0400

    Tested Calculator.show_history, clear_history, show_previous

--- tests/tests_calculator/test_calculator.py
+++ tests/tests_calculator/test_calculator.py
@@ -1,9 +1,11 @@
 # pylint: disable = expression-not-assigned
 # pylint: disable = too-many-arguments
+# pylint: disable = unused-argument
 
 '''Test Calculator'''
 import pytest
 from app.calculator import Calculator
+from app.calculator.history import History
 
 class TestCalculator():
     '''Tests Calculator.Operations'''
@@ -49,3 +51,27 @@ class TestCalculator():
         Calculator.calculate_and_print(a_string, b_string, operation_string)
         captured = capsys.readouterr()
         assert captured.out.strip() == expected_string, "Calculator Calculate and Print failed!"
+
+    @pytest.fixture
+    def setup_history(self):
+        '''Clears history and adds sample logs for tests'''
+        History.history.clear()
+        Calculator.add(2, 2)
+        Calculator.subtract(2, 2)
+
+    def test_show_history(self, setup_history, capsys):
+        '''Tests Calculator.show_history'''
+        Calculator.show_history()
+        captured = capsys.readouterr()
+        assert captured.out.strip() == "[(2, 2, 'add'), (2, 2, 'subtract')]", "Calculator Show History failed!"
+
+    def test_clear_history(self, setup_history):
+        '''Tests Calculator.clear_history'''
+        Calculator.clear_history()
+        assert len(History.history) == 0, "Calculator Clear History failed!"
+
+    def test_show_previous(self, setup_history, capsys):
+        '''Tests Calculator.show_previous'''
+        Calculator.show_previous()
+        captured = capsys.readouterr()
+        assert captured.out.strip() == "(2, 2, 'subtract')", "Calculator Show Previous failed!"
