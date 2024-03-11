231191889b8ac46bd0d45d41e7556b01305a2bbf
whua0 <wh44@njit.edu>
2024-03-10 22:45:09 -0400

    Added class TestIntroduction

--- tests/test_introduction.py
+++ tests/test_introduction.py
@@ -4,9 +4,12 @@
 import pytest
 from app.introduction import introduction
 
-def test_introduction(capsys):
-    '''Tests introduction'''
-    introduction()
-    captured = capsys.readouterr()
-    result = "Calculator App Initiated ...\nPlease type a command.\n"
-    assert captured.out == result, "Introduction Function failed!"
+class TestIntroduction():
+    '''Tests Introduction'''
+
+    def test_introduction(self, capsys):
+        '''Tests Introduction'''
+        introduction()
+        captured = capsys.readouterr()
+        result = "Calculator App Initiated ...\nPlease type a command.\n"
+        assert captured.out == result, "Introduction Function failed!"
