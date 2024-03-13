7a3e458950190df8154a4a606d1aa62b5694fbe0
whua0 <wh44@njit.edu>
2024-03-13 14:27:28 -0400

    Created CSVHandler Factory Method: check data directory, create csv file, delete csv file

---
+++ app/CSVHandler/__init__.py
@@ -0,0 +1,63 @@
+'''CSVHandler'''
+import os
+import pandas as pd
+import logging
+from app.calculator.history import History
+
+class CSVHandler:
+    '''Class CSVHandler'''
+    data_directory = "./data"
+    headers = ["Operator", "Operand_A", "Operand_B"]
+
+    @staticmethod
+    def check_data_directory():
+        '''Creates and checks if data directory is writable'''
+
+        if not os.path.exists(CSVHandler.data_directory):
+            os.makedirs(CSVHandler.data_directory)
+            logging.info(f"Created '{CSVHandler.data_directory}' directory.")
+
+        elif not os.access(CSVHandler.data_directory, os.W_OK):
+            print(f"An error occurred: Directory '{CSVHandler.data_directory}' is not writable.")
+            logging.error(f"Directory '{CSVHandler.data_directory}' is not writable.")
+            return
+
+    @staticmethod
+    def create_csv_file(filename):
+        '''Creates CSV File in data directory'''
+        filepath = os.path.join(CSVHandler.data_directory, filename)
+
+        if not os.path.exists(filepath):
+            df = pd.DataFrame(columns = CSVHandler.headers)
+            df.to_csv(filepath, index = True, index_label = "Index")
+            print(f"File '{filename}' has been created.")
+            logging.info(f"Created DataFrame: '{filename}'.")
+        else:
+            print(f"File '{filename}' already exists.")
+            logging.warning(f"Dataframe: '{filename}' already exists.")
+
+    @staticmethod
+    def delete_csv_file(filename):
+        '''Deletes CSV File in data directory'''
+        filepath = os.path.join(CSVHandler.data_directory, filename)
+
+        if os.path.exists(filepath):
+            os.remove(filepath)
+            print(f"File '{filename}' has been deleted.")
+            logging.info(f"Deleted Dataframe: '{filename}'.")
+        else:
+            print(f"File '{filename}' was not found.")
+            logging.warning(f"Dataframe: '{filename}' does not exists.")
+
+def CSV_Factory(operation):
+    '''CSV Factory Method'''
+
+    if operation == "check_directory":
+        return CSVHandler.check_data_directory
+    elif operation == "create":
+        return CSVHandler.create_csv_file
+    elif operation == "delete":
+        return CSVHandler.delete_csv_file
+    else:
+        logging.error(f"Failed to execute CSV Operation: {operation}.")
+        raise ValueError
