92f3b0e9527197d80f0cc1c9fc1c8672a2fd9901
whua0 <wh44@njit.edu>
2024-03-13 18:41:25 -0400

    Tested create_csv_file if file already exists & delete_csv_file if file does not exist

--- tests/test_csvhandler.py
+++ tests/test_csvhandler.py
@@ -25,7 +25,7 @@ class TestCSVHandler(unittest.TestCase):
     @patch("os.path.exists", return_value = False)
     @patch("os.makedirs")
     @patch("logging.info")
-    def test_check_data_directory_when_directory_not_exists(self, mock_logging_info, mock_makedirs, mock_exists):
+    def test_check_data_directory_if_directory_not_exists(self, mock_logging_info, mock_makedirs, mock_exists):
         '''Tests CSVHandler.check_data_directory if data directory does not exist'''
         csv_handler = CSVHandler()
         csv_handler.data_directory = "./data"
@@ -35,7 +35,7 @@ class TestCSVHandler(unittest.TestCase):
     @patch("os.access", return_value = False)
     @patch("sys.stdout", autospec = True)
     @patch("logging.error", autospec = True)
-    def test_check_data_directory_when_not_writable(self, mock_logging_error, mock_stdout, mock_access):
+    def test_check_data_directory_if_directory_not_writable(self, mock_logging_error, mock_stdout, mock_access):
         '''Tests CSVHandler.check_data_directory when directory is not writable'''
         csv_handler = CSVHandler()
         csv_handler.data_directory = "./data"
@@ -49,6 +49,15 @@ class TestCSVHandler(unittest.TestCase):
         CSVHandler.create_csv_file(filename)
         self.assertTrue(os.path.exists(os.path.join(self.test_dir, filename)))
 
+    @patch("logging.warning")
+    def test_create_csv_file_if_file_already_exists(self, mock_logging_warning):
+        '''Tests CSVHandler.create_csv_file if file already exists'''
+        CSVHandler.data_directory = self.test_dir
+        filename = "test.csv"
+        CSVHandler.create_csv_file(filename)
+        CSVHandler.create_csv_file(filename)
+        mock_logging_warning.assert_called_once_with(f"Dataframe: '{filename}' already exists.")
+
     def test_delete_csv_file(self):
         '''Tests CSVHanlder.delete_csv_file'''
         CSVHandler.data_directory = self.test_dir
@@ -57,6 +66,14 @@ class TestCSVHandler(unittest.TestCase):
         CSVHandler.create_csv_file(filename)
         CSVHandler.delete_csv_file(filename)
         self.assertFalse(os.path.exists(file_path))
+
+    @patch("logging.warning")
+    def test_delete_csv_file_if_file_not_exists(self, mock_logging_warning):
+        '''Tests CSVHanlder.delete_csv_file if file does not exist'''
+        CSVHandler.data_directory = self.test_dir
+        filename = "nofiletest.csv"
+        CSVHandler.delete_csv_file(filename)
+        mock_logging_warning.assert_called_once_with(f"Dataframe: '{filename}' does not exists.")
 
 if __name__ == '__main__':
     unittest.main() # pragma: no cover
