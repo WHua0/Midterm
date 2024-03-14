c01018485a79de0107ebeb81dac2b8617af1c394
whua0 <wh44@njit.edu>
2024-03-13 22:50:37 -0400

    Tested invalid inputs

---
+++ tests/tests_plugins/test_file_manager.py
@@ -0,0 +1,31 @@
+# pylint: disable = unused-argument
+# pylint: disable = unused-variable
+
+'''Test Add'''
+import unittest
+from unittest.mock import patch
+from app.plugins.file_manager import CSVCommand
+
+class TestCSVCommand(unittest.TestCase):
+    '''Tests File Manager: CSVCommand'''
+
+    @patch("builtins.input", side_effect=["3 5"])
+    @patch("logging.warning")
+    def test_execute_with_invalid_inputs(self, mock_logging_warning, mock_input):
+        '''Tests Execute_AddCommand with Invalid Inputs'''
+        command = CSVCommand()
+        with patch("builtins.print") as mock_print:
+            command.execute()
+            mock_logging_warning.assert_called_once_with("Invalid CSV Operation: 3.")
+
+    @patch("builtins.input", side_effect=["3"])
+    @patch("logging.warning")
+    def test_execute_with_incorrect_number_of_inputs(self, mock_logging_warning, mock_input):
+        '''Tests Execute_CSVCommand with Incorrect Number of Inputs'''
+        command = CSVCommand()
+        with patch("builtins.print") as mock_print:
+            command.execute()
+            mock_logging_warning.assert_called_once_with("Incorrect number of inputs: '%s'.", '3')
+
+if __name__ == '__main__':
+    unittest.main() # pragma: no cover
