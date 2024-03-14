fede2b2d762ba525b322ca2a69af1fd14739f915
whua0 <wh44@njit.edu>
2024-03-14 16:15:03 -0400

    Created showhistorycommand

---
+++ app/plugins/history/__init__.py
@@ -0,0 +1,22 @@
+'''history'''
+import logging
+import pandas as pd
+from app.commandhandler import Command
+from app.calculator.history import History
+
+class ShowHistoryCommand(Command):
+    '''ShowHistory Command'''
+
+    def __init__(self, history: History):
+        self.history = history
+
+    def execute(self):
+        history_data = self.history.retrieve_history()
+
+        if history_data == None:
+            print("No History!")
+            logging.info("No history available.")
+
+        else:
+            print("History:")
+            print(history_data)
