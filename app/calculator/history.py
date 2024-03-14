b10c287c01816e7ba0196822c15c29a89482b834
whua0 <wh44@njit.edu>
2024-03-14 14:27:04 -0400

    Created History.add_log

--- app/calculator/history.py
+++ app/calculator/history.py
@@ -12,3 +12,8 @@ class History:
         '''Converts inputs to a log'''
         log = {"Operation": operation.__name__, "OperandA": a, "OperandB": b}
         return log
+
+    def add_log(self, log):
+        '''Adds log to history'''
+        new_log = pd.DataFrame([log], columns=["Operation", "OperandA", "OperandB"])
+        self.history = pd.concat([self.history, new_log], ignore_index = True)
--- tests/tests_calculator/test_history.py
+++ tests/tests_calculator/test_history.py
@@ -14,5 +14,11 @@ class TestHistory(unittest.TestCase):
         expected_log = {"Operation": "add", "OperandA": 10, "OperandB": 5}
         self.assertEqual(log, expected_log)
 
+    def test_add_log(self):
+        '''Tests History.add_log'''
+        log = self.history.create_log(10, 5, Operation.add)
+        self.history.add_log(log)
+        self.assertEqual(len(self.history.history), 1)
+
 if __name__ == '__main__':
     unittest.main() # pragma: no cover
