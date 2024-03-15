9d11fc80b7efa54b3630d9090bd9fddbe35152f9
whua0 <wh44@njit.edu>
2024-03-14 22:37:21 -0400

    Tested HistoryHandler.save_history but failing to raise exception for raising invalid file names and nonexistence directories

--- tests/tests_historyhandler/test_historyhandler.py
+++ tests/tests_historyhandler/test_historyhandler.py
@@ -1,10 +1,15 @@
 '''Test HistoryHandler'''
+import os
 import unittest
 from app.calculator.history import History
 from app.historyhandler import HistoryHandler
 
 class TestHistoryHandler(unittest.TestCase):
     '''Tests HistoryHandler'''
+
+    def setUp(self):
+        self.history_instance = History()
+        self.filenames = []
 
     def test_create_history(self):
         '''Tests HistoryHandler.create_history'''
@@ -14,20 +19,43 @@ class TestHistoryHandler(unittest.TestCase):
 
     def test_retrieve_history(self):
         '''Tests HistoryHandler.retrieve_history'''
-        history_instance = History()
         log = {"Operation": "add", "OperandA": 1, "OperandB": 1}
-        history_instance.add_log(log)
-        expected_history = str(history_instance.get_history())
-        actual_history = str(HistoryHandler.retrieve_history(history_instance))
+        self.history_instance.add_log(log)
+        expected_history = str(self.history_instance.get_history())
+        actual_history = str(HistoryHandler.retrieve_history(self.history_instance))
         self.assertEqual(expected_history, actual_history)
 
     def test_clear_history(self):
         '''Tests HistoryHandler.clear_history'''
-        history_instance = History()
         log = {"Operation": "add", "OperandA": 1, "OperandB": 1}
-        history_instance.add_log(log)
-        HistoryHandler.clear_history(history_instance)
-        self.assertTrue(history_instance.get_history().empty)
+        self.history_instance.add_log(log)
+        HistoryHandler.clear_history(self.history_instance)
+        self.assertTrue(self.history_instance.get_history().empty)
+
+    def test_save_history(self):
+        '''Tests HistoryHandler.save_history'''
+        log = {"Operation": "add", "OperandA": 1, "OperandB": 1}
+        self.history_instance.add_log(log)
+        filename = "filename.csv"
+        self.filenames.append(filename)
+        HistoryHandler.save_history(self.history_instance, filename)
+        self.assertTrue(os.path.isfile(filename))
+        self.assertTrue(os.path.getsize(filename) > 0)
+
+    def test_save_history_invalid_filename(self):
+        '''Tests HistoryHandler.save_history'''
+        log = {"Operation": "add", "OperandA": 1, "OperandB": 1}
+        self.history_instance.add_log(log)
+        filename = "$invalidfilename.csv"
+        self.filenames.append(filename)
+        HistoryHandler.save_history(self.history_instance, filename)
+        with self.assertRaises(Exception):
+            HistoryHandler.save_history(self.history_instance, filename)
+
+    def tearDown(self):
+        for filename in self.filenames:
+            if os.path.exists(filename):
+                os.remove(filename)
 
 if __name__ == '__main__':
     unittest.main() # pragma: no cover
