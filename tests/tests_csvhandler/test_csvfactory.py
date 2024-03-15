6d90ba555d595843298b4b1df2a80393f67347ac
whua0 <wh44@njit.edu>
2024-03-15 13:30:06 -0400

    Tested load_csv_file_to_history if parsing error

--- tests/tests_csvhandler/test_csvfactory.py
+++ tests/tests_csvhandler/test_csvfactory.py
@@ -1,6 +1,7 @@
 '''Test CSVFactory'''
 import unittest
 import os
+import pandas as pd
 from unittest.mock import patch
 from app.csvhandler.csvfactory import CSVHandler
 from app.csvhandler.csvfilechecker import CSVFileChecker
@@ -69,6 +70,18 @@ class TestCSVHandler(unittest.TestCase):
         filepath = os.path.join(self.testdir, filename)
         CSVHandler.load_csv_file_to_history(filename, filepath)
         mock_logging_warning.assert_called_with(f"File '{filepath}' was not found.")
+
+    @patch("logging.error")
+    def test_load_csv_file_to_history_if_parsing_error(self, mock_logging_error):
+        '''Tests CSVHandler.load_csv_file_to_history if parsing error'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        with patch("pandas.read_csv") as mock_read_csv:
+            mock_read_csv.side_effect = pd.errors.ParserError("Error parsing CSV file")
+            CSVHandler.load_csv_file_to_history(filename, filepath)
+            mock_logging_error.assert_called_once_with(f"Failed to load history from CSV file '{filepath}': Error parsing CSV file")
 
     def tearDown(self):
         '''Deletes temporary test_data directory'''
