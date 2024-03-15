0efc1eb779ea145a946fddcee04c0818e2924055
whua0 <wh44@njit.edu>
2024-03-15 12:43:21 -0400

    Tested delete_csv_file

--- tests/tests_csvhandler/test_csvfactory.py
+++ tests/tests_csvhandler/test_csvfactory.py
@@ -32,6 +32,25 @@ class TestCSVHandler(unittest.TestCase):
         CSVHandler.save_history_to_csv_file(filename, filepath)
         mock_logging_warning.assert_called_once_with(f"File '{filepath}' already exists.")
 
+    @patch("logging.info")
+    def test_delete_csv_file(self, mock_logging_info):
+        '''Tests CSVFactory.delete_csv_file'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        CSVHandler.delete_csv_file(filename, filepath)
+        mock_logging_info.assert_called_with(f"Deleted File '{filename}'.") 
+
+    @patch("logging.warning")
+    def test_delete_csv_file_if_file_not_exists(self, mock_logging_warning):
+        '''Tests CSVFactory.delete_csv_file if files does not exist'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.delete_csv_file(filename, filepath)
+        mock_logging_warning.assert_called_with(f"File '{filepath}' was not found.")
+
     def tearDown(self):
         '''Deletes temporary test_data directory'''
         for filename in os.listdir(self.testdir):
