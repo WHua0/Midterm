e761456df147cdb41171c0a5fd235c09e69bf0e2
whua0 <wh44@njit.edu>
2024-03-15 16:06:49 -0400

    Created showfiles command

---
+++ app/plugins/showfiles/__init__.py
@@ -0,0 +1,27 @@
+'''ShowFiles'''
+import os
+import logging
+from app.commandhandler import Command
+from app.filehandler.csvfilechecker import CSVFileChecker
+
+class ShowFilesCommand(Command):
+    '''ShowFiles Command'''
+
+    def execute(self):
+
+        if not CSVFileChecker.check_data_directory():
+            return
+        
+        directory = CSVFileChecker.data_directory
+        
+        try:
+             filenames = os.listdir(directory)
+
+        except Exception as e:
+            print(f"Failed to list directory {directory}: {e}")
+            logging.error(f"Failed to list directory {directory}: {e}")
+            return
+
+        print("Files in Data Directory:")
+        for filename in filenames:
+            print(filename)
