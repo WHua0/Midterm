a23d966839e0877786eb0160ad68772aeb0b87bc
whua0 <wh44@njit.edu>
2024-03-15 13:51:08 -0400

    Removed previous code to use app.filehandler.csvfactory & updates CSVCommands

--- app/filehandler/__init__.py
+++ app/filehandler/__init__.py
@@ -1,75 +1,32 @@
-'''CSVHandler'''
-import os
-import pandas as pd
+'''FileHandler'''
 import logging
+from app.filehandler.csvfactory import CSVHandler
 from app.filehandler.csvfilechecker import CSVFileChecker
 
-class CSVHandler:
-    '''Class CSVHandler'''
-    headers = ["Operator", "Operand_A", "Operand_B"]
+class FileHandler:
+    '''Class FileHandler'''
 
     @staticmethod
-    def create_csv_file(filename, filepath):
-        '''Creates CSV File in data directory'''
+    def CSVCommands(operation, filename):
+        '''CSV Commands'''
 
-        if os.path.exists(filepath):
-            print(f"File '{filename}' already exists.")
-            logging.warning(f"File '{filename}' already exists.")
+        CSVFileChecker.check_data_directory()
+
+        if not CSVFileChecker.validate_filename(filename):
             return
 
-        df = pd.DataFrame(columns = CSVHandler.headers)
-        df.to_csv(filepath, index = True, index_label = "Index")
-        print(f"File '{filename}' has been created.")
-        logging.info(f"Created File '{filename}'.")
-  
+        filepath = CSVFileChecker.get_filepath(CSVFileChecker.data_directory, filename)
 
-    @staticmethod
-    def delete_csv_file(filename, filepath):
-        '''Deletes CSV File in data directory'''
-
-        if not os.path.exists(filepath):
-            print(f"File '{filename}' was not found.")
-            logging.warning(f"File '{filename}' does not exist.")
+        if not filepath:
             return
 
-        os.remove(filepath)
-        print(f"File '{filename}' has been deleted.")
-        logging.info(f"Deleted File '{filename}'.")
-    
-    @staticmethod
-    def clear_csv_file(filename, filepath):
-        '''Clears CSV File in data directory'''
-    
-        if not os.path.exists(filepath):
-            print(f"File '{filename}' was not found.")
-            logging.warning(f"File '{filename}' does not exist.")
+        if operation == "save":
+            return CSVHandler.save_history_to_csv_file(filename, filepath)
+        elif operation == "delete":
+            return CSVHandler.delete_csv_file(filename, filepath)
+        elif operation == "clear":
+            return CSVHandler.load_csv_file_to_history(filename, filepath)
+        else:
+            print(f"Invalid File Operation: {operation}.")
+            logging.warning(f"Invalid File Operation: {operation}.")
             return
-
-        df = pd.DataFrame(columns = CSVHandler.headers)
-        df.to_csv(filepath, index = True, index_label = "Index")
-        print(f"File '{filename}' has been cleared.")
-        logging.info(f"Cleared File '{filename}'.")
-
-def CSVFactory(operation, filename):
-    '''CSV Factory Method'''
-
-    CSVFileChecker.check_data_directory()
-
-    if not CSVFileChecker.validate_filename(filename):
-        return
-
-    filepath = CSVFileChecker.get_filepath(CSVFileChecker.data_directory, filename)
-
-    if filepath is None:
-        return
-
-    if operation == "create":
-        return CSVHandler.create_csv_file(filename, filepath)
-    elif operation == "delete":
-        return CSVHandler.delete_csv_file(filename, filepath)
-    elif operation == "clear":
-        return CSVHandler.clear_csv_file(filename, filepath)
-    else:
-        print(f"Invalid CSV Operation: {operation}.")
-        logging.warning(f"Invalid CSV Operation: {operation}.")
-        return None
