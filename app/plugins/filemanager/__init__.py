dca10fcc670e680d285a1e7937fb23428b21c80e
whua0 <wh44@njit.edu>
2024-03-15 13:51:50 -0400

    Updated to use FileHandler.CSVCommands

--- app/plugins/filemanager/__init__.py
+++ app/plugins/filemanager/__init__.py
@@ -7,13 +7,13 @@ class FileManagerCommand(Command):
     '''FileManager Command'''
 
     def execute(self):
-        print("FileManager Commands: create / delete / clear.")
-        values = input("Please provide a Command and File Name separated by a space: ").split()
+        print("FileManager Commands: save / delete / load.")
+        values = input("Please provide a Command and Filename separated by a space: ").split()
 
         if len(values) == 2:
             csvcommand, filename = values[0], values[1]
             logging.info("Inputs: '%s', '%s'.", csvcommand, filename)
-            FileHandler(csvcommand, filename)
+            FileHandler.CSVCommands(csvcommand, filename)
 
         else:
             print(f"An error occurred: incorrect number of inputs.")
