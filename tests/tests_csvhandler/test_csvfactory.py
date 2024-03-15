a488989b6f65b8dee43b87e6a9c120f4b5a481c5
whua0 <wh44@njit.edu>
2024-03-15 12:34:01 -0400

    Tested save_history if file already exists

--- tests/tests_csvhandler/test_csvfactory.py
+++ tests/tests_csvhandler/test_csvfactory.py
@@ -22,6 +22,16 @@ class TestCSVHandler(unittest.TestCase):
         CSVHandler.save_history_to_csv_file(filename, filepath)
         mock_logging_info.assert_called_once_with(f"Saved History to File '{filename}'.")
 
+    @patch("logging.warning")
+    def test_save_history_to_csv_file_if_file_already_exists(self, mock_logging_warning):
+        '''Tests CSVFactory.save_history_to_csv_file'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        mock_logging_warning.assert_called_once_with(f"File '{filepath}' already exists.")
+
     def tearDown(self):
         '''Deletes temporary test_data directory'''
         for filename in os.listdir(self.testdir):
