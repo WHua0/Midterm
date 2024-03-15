d9cfc8e447f809ba4d52210ff99a1a9030c345cc
whua0 <wh44@njit.edu>
2024-03-15 11:58:59 -0400

    Updated save history to save history in filepath

---
+++ app/csvhandler/csvfactory.py
@@ -0,0 +1,33 @@
+'''CSVFactory'''
+import os
+import logging
+import pandas as pd
+from app.csvhandler.csvfilechecker import CSVFileChecker
+from app.historyhandler import HistoryHandler
+
+class CSVHandler:
+    '''Factory Class CSVHandler'''
+
+    @staticmethod
+    def save_history_to_csv_file(filename, filepath):
+        '''Saves history to CSV File in directory'''
+
+        if CSVFileChecker.check_file_exists(filepath):
+            return
+
+        history_instance = HistoryHandler.create_history()
+        history_df = HistoryHandler.retrieve_history(history_instance)
+        history_df.to_csv(filepath, index = False)
+        print(f"Saved History to File '{filename}'.")
+        logging.info(f"Saved History to File '{filename}'.")
+
+    @staticmethod
+    def delete_csv_file(filename, filepath):
+        '''Deletes CSV File in directory'''
+
+        if CSVFileChecker.check_file_not_exists(filepath):
+            return
+
+        os.remove(filepath)
+        print(f"File '{filename}' has been deleted.")
+        logging.info(f"Deleted File '{filename}'.")
