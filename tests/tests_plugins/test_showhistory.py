164c36f8a87d46f0c0b5fc735be833f81536ca5c
whua0 <wh44@njit.edu>
2024-03-14 20:59:09 -0400

    Tested ShowHistoryCommand

--- tests/tests_plugins/test_showhistory.py
+++ tests/tests_plugins/test_showhistory.py
@@ -1,31 +1,17 @@
-'''Test Show History Plugin'''
+'''Test ShowHistory'''
 import unittest
-import sys
-from io import StringIO
-from unittest.mock import MagicMock
-from app.calculator.history import History
+from unittest.mock import patch
 from app.plugins.showhistory import ShowHistoryCommand
 
 class TestShowHistoryCommand(unittest.TestCase):
     '''Tests ShowHistoryCommand'''
 
-    def setUp(self):
-        self.mock_history = MagicMock(spec = History)
-        self.show_history_command = ShowHistoryCommand(self.mock_history)
-
-    def test_execute(self):
-        '''Tests Execute_ShowHistory'''
-        history_data = [
-            {"Operation": "add", "OperandA": 1, "OperandB": 1},
-        ]
-        self.mock_history.retrieve_history.return_value = history_data
-        captured_output = StringIO()
-        sys.stdout = captured_output
-        self.show_history_command.execute()
-        sys.stdout = sys.__stdout__
-        printed_output = captured_output.getvalue()
-        expected_output = "History:\n  Operation  OperandA  OperandB\n0       add         1         1\n"
-        self.assertEqual(printed_output, expected_output)
-
-if __name__ == "__main__":
-    unittest.main() # pragma: no cover
+    @patch("app.plugins.showhistory.HistoryHandler")
+    @patch("app.plugins.showhistory.logging")
+    def test_execute(self, mock_logging, mockhistoryhandler):
+        '''Tests Execute_ShowHistory with empty history'''
+        history_data = []
+        mockhistoryhandler.return_value.retrieve_history.return_value = history_data
+        command = ShowHistoryCommand()
+        command.execute()
+        mock_logging.info.assert_called_once_with("Printed History:\nEmpty DataFrame\nColumns: []\nIndex: []")
