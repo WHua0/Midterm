1b6585ce16da77b38795ecb23b576ae61878a741
whua0 <wh44@njit.edu>
2024-03-14 23:56:15 -0400

    Removed HistoryHandler.save_history, moving save_history to different class

--- tests/tests_historyhandler/test_historyhandler.py
+++ tests/tests_historyhandler/test_historyhandler.py
@@ -1,5 +1,4 @@
 '''Test HistoryHandler'''
-import os
 import unittest
 from app.calculator.history import History
 from app.historyhandler import HistoryHandler
@@ -9,7 +8,6 @@ class TestHistoryHandler(unittest.TestCase):
 
     def setUp(self):
         self.history_instance = History()
-        self.filenames = []
 
     def test_create_history(self):
         '''Tests HistoryHandler.create_history'''
@@ -31,31 +29,6 @@ class TestHistoryHandler(unittest.TestCase):
         self.history_instance.add_log(log)
         HistoryHandler.clear_history(self.history_instance)
         self.assertTrue(self.history_instance.get_history().empty)
-
-    def test_save_history(self):
-        '''Tests HistoryHandler.save_history'''
-        log = {"Operation": "add", "OperandA": 1, "OperandB": 1}
-        self.history_instance.add_log(log)
-        filename = "filename.csv"
-        self.filenames.append(filename)
-        HistoryHandler.save_history(self.history_instance, filename)
-        self.assertTrue(os.path.isfile(filename))
-        self.assertTrue(os.path.getsize(filename) > 0)
-
-    def test_save_history_invalid_filename(self):
-        '''Tests HistoryHandler.save_history'''
-        log = {"Operation": "add", "OperandA": 1, "OperandB": 1}
-        self.history_instance.add_log(log)
-        filename = "$invalidfilename.csv"
-        self.filenames.append(filename)
-        HistoryHandler.save_history(self.history_instance, filename)
-        with self.assertRaises(Exception):
-            HistoryHandler.save_history(self.history_instance, filename)
-
-    def tearDown(self):
-        for filename in self.filenames:
-            if os.path.exists(filename):
-                os.remove(filename)
 
 if __name__ == '__main__':
     unittest.main() # pragma: no cover
