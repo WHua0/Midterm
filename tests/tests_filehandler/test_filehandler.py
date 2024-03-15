d64073d433d50acbea6cd81655c5e50b5f41ee5b
whua0 <wh44@njit.edu>
2024-03-15 18:15:41 -0400

    Forced test to use test_data directory instead of data directory

--- tests/tests_filehandler/test_filehandler.py
+++ tests/tests_filehandler/test_filehandler.py
@@ -5,19 +5,22 @@ import unittest
 import os
 from unittest.mock import patch
 from app.filehandler import FileHandler
+from app.filehandler.csvfilechecker import CSVFileChecker
 
 
 class TestFileHandler(unittest.TestCase):
     '''Tests FileHandler'''
 
-    def tearDown(self):
-        filename = "data/test.csv"
-        if os.path.exists(filename):
-            os.remove(filename)
+    def setUp(self):
+        '''Creates temporary test_data directory'''
+        self.testdir = "./test_data"
+        os.makedirs(self.testdir, exist_ok = True)
+
 
     @patch("logging.warning")
     def test_invalid_filename(self, mock_logging_warning):
         '''Tests FileHandler.CVSCommands with invalid filename'''
+        CSVFileChecker.data_directory = self.testdir
         filename = "$test.csv"
         FileHandler.CSVCommands("save", filename)
         mock_logging_warning.assert_called_once_with(f"Invalid File Name: {filename}.")
@@ -28,13 +31,15 @@ class TestFileHandler(unittest.TestCase):
     @patch("logging.error", autospec = True)
     def test_not_writable_filename(self, mock_logging_error, mock_stdout, mock_access, mock_exists):
         '''Tests FileHandler.CVSCommands with unwritable file'''
+        CSVFileChecker.data_directory = self.testdir
         filename = "test.csv"
         FileHandler.CSVCommands("save", filename)
-        mock_logging_error.assert_called_with("File './data/test.csv' is not writable.")
+        mock_logging_error.assert_called_with("File './test_data/test.csv' is not writable.")
 
     @patch("logging.info")
     def test_save_csv_file(self, mock_logging_info):
         '''Tests FileHandler.CVSCommands Save'''
+        CSVFileChecker.data_directory = self.testdir
         filename = "test.csv"
         FileHandler.CSVCommands("save", filename)
         mock_logging_info.assert_called_once_with(f"Saved History to File '{filename}'.")
@@ -42,6 +47,7 @@ class TestFileHandler(unittest.TestCase):
     @patch("logging.info")
     def test_delete_csv_file(self, mock_logging_info):
         '''Tests FileHandler.CVSCommands Delete'''
+        CSVFileChecker.data_directory = self.testdir
         filename = "test.csv"
         FileHandler.CSVCommands("save", filename)
         FileHandler.CSVCommands("delete", filename)
@@ -50,6 +56,7 @@ class TestFileHandler(unittest.TestCase):
     @patch("logging.info")
     def test_load_csv_file(self, mock_logging_info):
         '''Tests FileHandler.CVSCommands Load'''
+        CSVFileChecker.data_directory = self.testdir
         filename = "test.csv"
         FileHandler.CSVCommands("save", filename)
         FileHandler.CSVCommands("load", filename)
@@ -58,9 +65,18 @@ class TestFileHandler(unittest.TestCase):
     @patch("logging.warning")
     def test_invalid_command(self, mock_logging_warning):
         '''Tests FileHandler.CVSCommands with invalid command'''
+        CSVFileChecker.data_directory = self.testdir
         filename = "test.csv"
         FileHandler.CSVCommands("invalid", filename)
         mock_logging_warning.assert_called_with("Invalid File Operation: invalid.")
+
+    def tearDown(self):
+        '''Deletes temporary test_data directory'''
+        for filename in os.listdir(self.testdir):
+            filepath = os.path.join(self.testdir, filename)
+            if os.path.isfile(filepath):
+                os.remove(filepath)
+        os.rmdir(self.testdir)
 
 if __name__ == "__main__":
     unittest.main() # pragma: no cover
