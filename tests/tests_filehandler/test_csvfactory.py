b1052feddc5f9e4d8135807ebfb35b429ff612cd
whua0 <wh44@njit.edu>
2024-03-15 13:35:10 -0400

    Changed file name from CSVhandler to filehandler for consistency

---
+++ tests/tests_filehandler/test_csvfactory.py
@@ -0,0 +1,95 @@
+'''Test CSVFactory'''
+import unittest
+import os
+from unittest.mock import patch
+import pandas as pd
+from app.filehandler.csvfactory import CSVHandler
+from app.filehandler.csvfilechecker import CSVFileChecker
+
+class TestCSVHandler(unittest.TestCase):
+    '''Tests CSVFactory'''
+
+    def setUp(self):
+        '''Creates temporary test_data directory'''
+        self.testdir = "./test_data"
+        os.makedirs(self.testdir, exist_ok = True)
+
+    @patch("logging.info")
+    def test_save_history_to_csv_file(self, mock_logging_info):
+        '''Tests CSVFactory.save_history_to_csv_file'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        mock_logging_info.assert_called_once_with(f"Saved History to File '{filename}'.")
+
+    @patch("logging.warning")
+    def test_save_history_to_csv_file_if_file_already_exists(self, mock_logging_warning):
+        '''Tests CSVFactory.save_history_to_csv_file'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        mock_logging_warning.assert_called_once_with(f"File '{filepath}' already exists.")
+
+    @patch("logging.info")
+    def test_delete_csv_file(self, mock_logging_info):
+        '''Tests CSVFactory.delete_csv_file'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        CSVHandler.delete_csv_file(filename, filepath)
+        mock_logging_info.assert_called_with(f"Deleted File '{filename}'.")
+
+    @patch("logging.warning")
+    def test_delete_csv_file_if_file_not_exists(self, mock_logging_warning):
+        '''Tests CSVFactory.delete_csv_file if file does not exist'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.delete_csv_file(filename, filepath)
+        mock_logging_warning.assert_called_with(f"File '{filepath}' was not found.")
+
+    @patch("logging.info")
+    def test_load_csv_file_to_history(self, mock_logging_info):
+        '''Tests CSVFactory.load_csv_file_to_history'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        CSVHandler.load_csv_file_to_history(filename, filepath)
+        mock_logging_info.assert_called_with(f"Loaded history from File '{filename}'.")
+
+    @patch("logging.warning")
+    def test_load_csv_file_to_history_if_file_not_exists(self, mock_logging_warning):
+        '''Tests CSVFactory.load_csv_file_to_history if file does not exist'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.load_csv_file_to_history(filename, filepath)
+        mock_logging_warning.assert_called_with(f"File '{filepath}' was not found.")
+
+    @patch("logging.error")
+    def test_load_csv_file_to_history_if_parsing_error(self, mock_logging_error):
+        '''Tests CSVHandler.load_csv_file_to_history if parsing error'''
+        CSVFileChecker.data_directory = self.testdir
+        filename = "test.csv"
+        filepath = os.path.join(self.testdir, filename)
+        CSVHandler.save_history_to_csv_file(filename, filepath)
+        with patch("pandas.read_csv") as mock_read_csv:
+            mock_read_csv.side_effect = pd.errors.ParserError("Error parsing CSV file")
+            CSVHandler.load_csv_file_to_history(filename, filepath)
+            mock_logging_error.assert_called_once_with(f"Failed to load history from CSV file '{filepath}': Error parsing CSV file")
+
+    def tearDown(self):
+        '''Deletes temporary test_data directory'''
+        for filename in os.listdir(self.testdir):
+            filepath = os.path.join(self.testdir, filename)
+            if os.path.isfile(filepath):
+                os.remove(filepath)
+        os.rmdir(self.testdir)
+
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
---
+++ tests/tests_filehandler/test_csvfilechecker.py
@@ -0,0 +1,117 @@
+# pylint: disable = unused-argument
+
+'''Test CSV File Checker'''
+import unittest
+import os
+from unittest.mock import patch
+from app.filehandler.csvfilechecker import CSVFileChecker
+
+class TestCSVFileChecker(unittest.TestCase):
+    '''Tests CSV File Checker'''
+
+    @patch("os.path.exists", return_value = False)
+    @patch("os.makedirs")
+    @patch("logging.info")
+    def test_check_data_directory_if_directory_not_exists(self, mock_logging_info, mock_makedirs, mock_exists):
+        '''Tests CSVFileChecker.check_data_directory if data directory does not exist'''
+        CSVFileChecker.data_directory = "./data"
+        CSVFileChecker.check_data_directory()
+        mock_logging_info.assert_called_once_with(f"Created '{CSVFileChecker.data_directory}' directory.")
+
+    @patch("os.path.exists", return_value = True)
+    @patch("os.access", return_value = False)
+    @patch("sys.stdout", autospec = True)
+    @patch("logging.error", autospec = True)
+    def test_check_data_directory_if_directory_not_writable(self, mock_logging_error, mock_stdout, mock_access, mock_exists):
+        '''Tests CSVFileChecker.check_data_directory if data directory is not writable'''
+        CSVFileChecker.data_directory = "./data"
+        CSVFileChecker.check_data_directory()
+        mock_logging_error.assert_called_once_with(f"Directory '{CSVFileChecker.data_directory}' is not writable.")
+
+    @patch("os.path.exists", return_value = True)
+    @patch("os.access", return_value = True)
+    def test_check_file_writable_if_writable(self, mock_access, mock_exists):
+        '''Tests CSVFileChecker.check_file_writable if file is writable'''
+        directory = "data"
+        filename = "test_file.csv"
+        expected_filepath = os.path.join(directory, filename)
+        result = CSVFileChecker.check_file_writable(expected_filepath)
+        self.assertTrue(result)
+
+    @patch("os.path.exists", return_value = True)
+    @patch("os.access", return_value = False)
+    @patch("sys.stdout", autospec = True)
+    @patch("logging.error", autospec = True)
+    def test_check_file_writable_if_not_writable(self, mock_logging_error, mock_stdout, mock_access, mock_exists):
+        '''Tests CSVFileChecker.check_file_writable if file is not writable'''
+        directory = "data"
+        filename = "test_file.csv"
+        expected_filepath = os.path.join(directory, filename)
+        CSVFileChecker.check_file_writable(expected_filepath)
+        mock_logging_error.assert_called_once_with(f"File '{expected_filepath}' is not writable.")
+
+    @patch("os.path.exists", return_value = True)
+    def test_check_file_exists_if_exists(self, mock_exist):
+        '''Tests CSVFileChecker.check_file_exists if file exists'''
+        filepath = "existing/file.txt"
+        self.assertTrue(CSVFileChecker.check_file_exists(filepath))
+
+    def test_check_file_exists_if_not_exists(self):
+        '''Tests CSVFileChecker.check_file_exists if file does not exist'''
+        filepath = "non_existing/file.txt"
+        self.assertFalse(CSVFileChecker.check_file_exists(filepath))
+
+    def test_check_file_not_exists_if_not_exists(self):
+        '''Tests CSVFileChecker.check_file_not_exists if file does not exist'''
+        filepath = "non_existing/file.txt"
+        self.assertTrue(CSVFileChecker.check_file_not_exists(filepath))
+
+    @patch("os.path.exists", return_value = True)
+    def test_check_file_not_exists_if_exists(self, mock_exist):
+        '''Tests CSVFileChecker.check_file_not_exists if file exists'''
+        filepath = "existing/file.txt"
+        self.assertFalse(CSVFileChecker.check_file_not_exists(filepath))
+
+    def test_validate_filename(self):
+        '''Tests CSVFileChecker.validate_filename if valid filename'''
+        valid_filename = "valid_file.csv"
+        self.assertTrue(CSVFileChecker.validate_filename(valid_filename))
+
+    def test_validate_filename_if_invalid_filename(self):
+        '''Tests CSVFileChecker.validate_filename if invalid filename'''
+        invalid_filename = "$invalidfile.csv"
+        self.assertFalse(CSVFileChecker.validate_filename(invalid_filename))
+
+    def test_validate_filename_if_filepath(self):
+        '''Tests CSVFileChecker.validate_filename if filepath'''
+        invalid_filename = "file/path.csv"
+        self.assertFalse(CSVFileChecker.validate_filename(invalid_filename))
+
+    def test_get_filepath(self):
+        '''Tests CSVFileChecker.get_filepath'''
+        directory = "data"
+        filename = "test_file.csv"
+        expected_filepath = os.path.join(directory, filename)
+        self.assertEqual(CSVFileChecker.get_filepath(directory, filename), expected_filepath)
+
+    def test_get_filepath_if_file_missing_csv(self):
+        '''Tests CSVFileChecker.get_filepath if missing .csv'''
+        directory = "data"
+        filename = "test_file"
+        expected_filepath = os.path.join(directory, filename + ".csv")
+        self.assertEqual(CSVFileChecker.get_filepath(directory, filename), expected_filepath)
+
+    @patch("os.path.exists", return_value = True)
+    @patch("os.access", return_value = False)
+    @patch("sys.stdout", autospec = True)
+    @patch("logging.error", autospec = True)
+    def test_get_filepath_if_file_not_writable(self, mock_logging_error, mock_stdout, mock_access, mock_exists):
+        '''Tests CSVFileChecker.get_filepath if file is not writable'''
+        directory = "data"
+        filename = "test_file.csv"
+        expected_filepath = os.path.join(directory, filename)
+        CSVFileChecker.get_filepath(directory, filename)
+        mock_logging_error.assert_called_once_with(f"File '{expected_filepath}' is not writable.")
+
+if __name__ == '__main__':
+    unittest.main() # pragma: no cover
