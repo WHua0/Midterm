f054e5d12334a6a424925d40f7a03f6d9b2d16a5
whua0 <wh44@njit.edu>
2024-03-21 21:55:45 -0400

    Updated logging

--- app/commandhandler/__init__.py
+++ app/commandhandler/__init__.py
@@ -19,17 +19,17 @@ class CommandHandler:
     def register_command(self, command_name: str, command: Command):
         '''Registers the Plugin Command'''
         self.commands[command_name] = command
-        logging.info("Registered Command: '%s'.", command_name)
 
     def execute_command(self, command_name: str):
         '''Executes the Command'''
         try:
-            logging.info("Attempted Command: '%s'.", command_name)
+            logging.info(f"Initiated Command: {command_name}.")
             self.commands[command_name].execute()
-            logging.info("Executed Command: '%s'.", command_name)
+            logging.info(f"Completed Command: {command_name}.")
 
-        except KeyError:
-            logging.warning("Invalid Command: '%s'.", command_name)
+        except KeyError as e:
             print(f"Invalid Command: {command_name}")
+            logging.error(f"Exception {e}, Invalid Command {command_name}.")
             print("Type 'menu' for list of commands.")
             print("Type 'exit' to exit.")
+
